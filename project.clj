f8cba40aa0d9aff6101510186ac485911910db36
Sebastiansen <sebastian@acid.cl>
2012-10-26 16:10:34 -0300

    new release

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject org.clojars.acidlabs/korma "0.3.2"
+(defproject org.clojars.acidlabs/korma "0.4.0"
   :description "Tasty SQL for Clojure"
   :url "https://github.com/acidlabs/Korma"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [c3p0/c3p0 "0.9.1.2"]
-                 [org.clojure/java.jdbc "0.2.2"]
-                 [postgresql "9.0-801.jdbc4"]]
+                 [org.clojure/java.jdbc "0.2.2"]]
   :codox {:exclude [korma.sql.engine korma.sql.fns korma.sql.utils]}
-          :dev-dependencies [[postgresql "9.0-801.jdbc4"]])
+  :dev-dependencies [[postgresql "9.0-801.jdbc4"]])
